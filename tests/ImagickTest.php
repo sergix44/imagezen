7ed57eb5b196353c35f94a85910e707b1f64a692
SergiX44 <SergiX44@users.noreply.github.com>
2023-09-11 22:38:23 +0000

    fix code style

--- tests/ImagickTest.php
+++ tests/ImagickTest.php
@@ -3,7 +3,7 @@
 use SergiX44\ImageZen\Backend;
 use SergiX44\ImageZen\Image;
 
-beforeEach()->skip(fn() => !extension_loaded('imagick'), 'imagick extension not loaded.');
+beforeEach()->skip(fn () => !extension_loaded('imagick'), 'imagick extension not loaded.');
 
 it('can blur an image', function ($file) {
     $filename = 'baboon_blur';
@@ -32,4 +32,3 @@ it('can change brightness of an image', function ($file) {
 
     unlink($out);
 })->with('baboon')->skip();
-
